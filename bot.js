3b3342f1299c7a00d0db2093a402dfbe899d89ad
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-15 19:35:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,4 +73,24 @@ client.on('message', message =>{
         }
     };
 });
+client.on('message', message => {
+  let command = message.content.split(" ")[0].slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+
+  if(!message.content.toLowerCase().startsWith(prefix)) return;
+  if(command == "suggest") {
+    if(!args.join(" ")) return message.reply(`${prefix}suggest <suggestion>`);
+    let channel = message.guild.channels.find(c => c.name == "suggestions");
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.username, message.author.displayAvatarURL)
+    .setTitle(`New Suggestion!`)
+    .setFooter(message.author.id)
+    .setDescription(args.join(" "));
+    channel.send(embed).then(msg => {
+      msg.react("✅").then(() => msg.react("❌"));
+      message.delete()
+      message.channel.send(`Success!, your suggestion has been recoded to <#${channel.id}>`);
+    });
+  }
+});
 client.login(process.env.BOT_TOKEN);
