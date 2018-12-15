8046e6065e9076562b6c1bef6820b62f599c3c4d
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-15 20:39:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -309,4 +309,13 @@ client.on("guildMemberAdd", member => {
 انت العضو رقم ${member.guild.memberCount} `) 
 }).catch(console.error)
 })
+client.on("message", (message) => {
+if (message.content.startsWith("$cv")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'voice');
+    message.channel.sendMessage('تـم إنـشاء روم صـوتي')
+    
+}
+});
 client.login(process.env.BOT_TOKEN);
