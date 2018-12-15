e57c1b14b5c657592108599d48dec5eb58e637fd
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-15 20:27:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -252,7 +252,7 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 });
 client.on('message', message => {
                                 if(!message.channel.guild) return;
-                        if (message.content.startsWith('ping')) {
+                        if (message.content.startsWith('$ping')) {
                             if(!message.channel.guild) return;
                             var msg = `${Date.now() - message.createdTimestamp}`
                             var api = `${Math.round(client.ping)}`
