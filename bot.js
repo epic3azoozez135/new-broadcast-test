861e3d5d5257b4ede28c0f939c9080b122d94bee
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-15 20:08:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -200,4 +200,28 @@ message.channel.send("``لا تستطيع سحب "+ message.mentions.members.fir
 } else {
 message.react("❌")
  }}});
+const invites = {};
+
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+  wait(1000);
+
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const logChannel = member.guild.channels.find(channel => channel.name === "eight-chat");
+    logChannel.send(`${member} Invited by: <@${inviter.id}>`);
+  });
+});
 client.login(process.env.BOT_TOKEN);
