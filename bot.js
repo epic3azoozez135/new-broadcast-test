57add85a9f132d8230422a7a386b8159f38ab100
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-17 18:26:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1272,19 +1272,28 @@ client.on('message', async message => {
   }
 });
 
+   client.on('message', msg => {
+  if(msg.content === '$hide') {
+    msg.guild.channels.forEach(c => {
+      c.overwritePermissions(msg.guild.id, {
+        SEND_MESSAGES: false,
+        READ_MESSAGES: false
+      })
+    })
+    msg.channel.send('.')
+  }
+})   
+
 client.on('message', msg => {
+  if(msg.content === '$show') {
+    msg.guild.channels.forEach(c => {
+      c.overwritePermissions(msg.guild.id, {
+        SEND_MESSAGES: true,
+        READ_MESSAGES: true
+      })
+    })
+    msg.channel.send('.')
+  }
+}) 
 
-    if (msg.content == '$join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
-client.on('ready', () => {
-    client.channels.get("522455377963646998").join(); 
-    });
- 
 client.login(process.env.BOT_TOKEN);
