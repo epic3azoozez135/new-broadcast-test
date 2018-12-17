2e30d0c9692801d2b66a7476eb960048ca808bc0
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-17 20:13:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1386,4 +1386,123 @@ client.on('guildMemberAdd', member => {
   }
 });
 
+
+	client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('كسمك')){
+        message.delete()
+      message.channel.sendMessage("", {embed: {
+        title: "لا تسب",
+        color: 0x06DF00,
+        description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
+        footer: {
+          text: "By Abo Khalil"
+        }
+      }}).then(msg => {msg.delete(4000)});
+                          }
+
+     
+}); 
+
+
+
+
+	client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('امك')){
+        message.delete()
+      message.channel.sendMessage("", {embed: {
+        title: "لا تسب",
+        color: 0x06DF00,
+        description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
+        footer: {
+          text: "By Abo Khalil"
+        }
+      }}).then(msg => {msg.delete(4000)});
+                          }
+
+     
+}); 
+
+
+
+
+	client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('عرص')){
+        message.delete()
+      message.channel.sendMessage("", {embed: {
+        title: "لا تسب",
+        color: 0x06DF00,
+        description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
+        footer: {
+          text: "By Abo Khalil"
+        }
+      }}).then(msg => {msg.delete(4000)});
+                          }
+
+     
+}); 
+
+
+
+
+	client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('خول')){
+        message.delete()
+      message.channel.sendMessage("", {embed: {
+        title: "لا تسب",
+        color: 0x06DF00,
+        description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
+        footer: {
+          text: "By Abo Khalil"
+        }
+      }}).then(msg => {msg.delete(4000)});
+                          }
+
+     
+}); 
+
+
+
+
+	client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('شرموط')){
+        message.delete()
+      message.channel.sendMessage("", {embed: {
+        title: "لا تسب",
+        color: 0x06DF00,
+        description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
+        footer: {
+          text: "By Abo Khalil"
+        }
+      }}).then(msg => {msg.delete(4000)});
+                          }
+
+     
+}); 
+
+
+
+
+	client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('متناك')){
+        message.delete()
+      message.channel.sendMessage("", {embed: {
+        title: "لا تسب",
+        color: 0x06DF00,
+        description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
+        footer: {
+          text: "By Abo Khalil"
+        }
+      }}).then(msg => {msg.delete(4000)});
+                          }
+
+     
+}); 
+
+
 client.login(process.env.BOT_TOKEN);
