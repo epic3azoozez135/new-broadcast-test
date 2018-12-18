aacc1267c9d00d8c0919b71f24d8b9a9f9001e5f
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-18 16:48:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1653,5 +1653,44 @@ client.on('message',async message => {
   }
 });
 
+client.on('message',function(message) {
+    let w = ['Rock','Paper','Scissors'];
+   if(message.content.startsWith(prefix + "rps")) {
+       message.channel.send(` \`\`\`css
+Choose one of the following.
+#1 ( حجره )
+#2 ( ورقة )
+#3 ( مقص )
+\`\`\`
+__امامك  5 توان للاختيار__`)
+.then(() => {
+  message.channel.awaitMessages(response => response.content === '1', {
+    max: 1,
+    time: 5000,
+    errors: ['time'],
+  })
+  .then((collected) => {
+      if(message.author !== message.author)return;
+     message.channel.send(':rosette: ' + w[Math.floor(Math.random() * w.length)]);
+    });
+});
+  message.channel.awaitMessages(response => response.content === '2', {
+    max: 1,
+    time: 5000,
+    errors: ['time'],
+  })
+  .then((collected) => {
+     message.channel.send(':rosette: ' + w[Math.floor(Math.random() * w.length)]);
+    });
+      message.channel.awaitMessages(response => response.content === '3', {
+    max: 1,
+    time: 5000,
+    errors: ['time'],
+  })
+  .then((collected) => {
+     message.channel.send(':rosette: ' + w[Math.floor(Math.random() * w.length)]);
+    });
+   } 
+});
 
 client.login(process.env.BOT_TOKEN);
